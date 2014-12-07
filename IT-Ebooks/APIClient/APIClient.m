5f6297aa39737d15ebac1bc78b14924d6fb53859
ac <cas0001a@c700470.local>
2014-12-08 00:40:46 +0100

    adding parseBooks:onSuccess:onFailure method

--- IT-Ebooks/APIClient/APIClient.m
+++ IT-Ebooks/APIClient/APIClient.m
@@ -36,18 +36,31 @@ static const NSString *BASE_URL = @"http://it-ebooks-api.info/v1";
     return _sharedInstance;
 }
 
+- (void)parseBooks:(NSDictionary *)json onSuccess:(void (^)(Books *results))success onFailure:(void(^)(NSError *error))failure {
+    NSError *jsonError = nil;
+    Books *results = [[Books alloc] initWithDictionary:json error:&jsonError];
+    if (!jsonError) {
+        if ([results.error isEqualToString:@"0"]) {
+            success(results);
+        } else {
+          failure([NSError errorWithDomain:@"Response succeed with error." code:400 userInfo:@{@"Error Response:":results.error}]);
+        }
+    } else {
+        failure(jsonError);
+    }
+}
+
 - (void)searchByQuery:(NSString *)query onSuccess:(void (^)(Books *results))success onFailure:(void (^)(NSError *error))failure {
     NSString *urlPath = [NSString stringWithFormat:@"%@/search/%@", BASE_URL, query];
     [httpClient GET:urlPath withSuccess:^(NSDictionary *json) {
-        Books *results = [[Books alloc] initWithDictionary:json error:NULL];
-        success(results);
+        [self parseBooks:json onSuccess:success onFailure:failure];
     } onFailure:failure];
 }
 
 - (void)searchByQuery:(NSString *)query onPage:(int)page onSuccess:(void (^)(Books *books))success onFailure:(void (^)(NSError *error))failure {
     NSString *urlPath = [NSString stringWithFormat:@"%@/search/%@/page/%i", BASE_URL, query, page];
     [httpClient GET:urlPath withSuccess:^(NSDictionary *json) {
-        success([[Books alloc] initWithDictionary:json error:NULL]);
+        [self parseBooks:json onSuccess:success onFailure:failure];
     } onFailure:failure];
 }
 
