158be737a21142faaebb0f6b193dec8d378f9549
aldocastro <aldocastro@host.local>
2015-01-12 23:08:40 +0100

    complete refactor of Books model class

--- IT-Ebooks/APIClient/Books.h
+++ IT-Ebooks/APIClient/Books.h
@@ -3,39 +3,40 @@
 // Copyright (c) 2014 Aldo Castro. All rights reserved.
 //
 
-@import UIKit;
-#import <JSONModel/JSONModel.h>
+@import Foundation;
 
-/*
- Error	Error code / description (Note: request success code = 0)
- Time	Request query execution time (seconds)
- ID	The ID of the book
- Title	The title of the book
- SubTitle	The subtitle of the book
- Description	The description of the book
- Author	The author(s) name of the book
- ISBN	The International Standard Book Number (ISBN) of the book
- Page	The number of pages of the book
- Year	The publication date (year) of the book
- Publisher	The publisher of the book
- Image	The image URL of the book
- Download	The download URL of the book
- */
-
-@protocol Book @end
-@interface Book : JSONModel
-@property (nonatomic, strong) NSString *mDescription;
-@property (nonatomic, strong) NSString *mBookId;
-@property (nonatomic, strong) NSURL *mImage;
-@property (nonatomic, strong) NSString <Optional> *mSubTitle;
-@property (nonatomic, strong) NSString *mTitle;
-@property (nonatomic, strong) NSString *mIsbn;
--(UIImage *)getImage;
+@interface BookDetails : NSObject
+@property (nonatomic, strong) NSString *ID;
+@property (nonatomic, strong) NSString *ISBN;
+@property (nonatomic, strong) NSString *Title;
+@property (nonatomic, strong) NSString *SubTitle;
+@property (nonatomic, strong) NSString *Description;
+@property (nonatomic, strong) NSURL *Image;
+@property (nonatomic, strong) NSString *Author;
+@property (nonatomic, strong) NSString *Page;
+@property (nonatomic, strong) NSString *Year;
+@property (nonatomic, strong) NSString *Publisher;
+@property (nonatomic, strong) NSString *Download;
+@property (nonatomic, strong) NSError *Error;
+@property (nonatomic, strong) NSNumber *Time;
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary error:(NSError **)error;
 @end
 
-@interface Books : JSONModel
-@property (nonatomic, strong) NSArray<Optional, Book> *books;
-@property (nonatomic, assign) NSString <Optional> *error;
-@property (nonatomic, assign) NSString <Optional> *total;
-@property (nonatomic, assign) NSString <Optional> *page;
+@interface Books : NSObject
+@property (nonatomic, strong) NSString *ID;
+@property (nonatomic, strong) NSString *ISBN; // optional
+@property (nonatomic, strong) NSString *Title;
+@property (nonatomic, strong) NSString *SubTitle; // optional
+@property (nonatomic, strong) NSString *Description;
+@property (nonatomic, strong) NSURL *Image;
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary error:(NSError **)error;
+@end
+
+@interface BookSearch : NSObject
+@property (nonatomic, strong) NSSet *Books;
+@property (nonatomic, strong) NSError *Error;
+@property (nonatomic, strong) NSString *Total;
+@property (nonatomic, strong) NSNumber *Page;
+@property (nonatomic, strong) NSNumber *Time;
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary error:(NSError **)error;
 @end
