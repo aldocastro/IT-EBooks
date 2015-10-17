8eeb715c53e720a353f42b5ef6f7608b5eca4db3
Aldo Castro <castro.aldo@live.com>
2015-10-17 17:02:17 +0200

    a bit of refactoring in the cache manager

--- IT-Ebooks/CacheManager.m
+++ IT-Ebooks/CacheManager.m
@@ -32,15 +32,36 @@
     return shared;
 }
 
+- (BOOL)isImageDataInCacheWithIdentifier:(NSString *)identifier {
+    @try {
+        NSData *cachedImageData = [_cache objectForKey:identifier];
+        return cachedImageData && [cachedImageData isKindOfClass:[NSData class]];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"exception: %@", exception);
+    }
+}
+
 - (void)cacheImageData:(NSData *)imageData withIdentifier:(NSString *)identifier {
-    if (!imageData && identifier && identifier.length<1) return;
-    [_cache setObject:imageData forKey:identifier];
+    if (!imageData && identifier && identifier.length<1 && ![self isImageDataInCacheWithIdentifier:identifier]) return;
+    @try {
+        [_cache setObject:imageData forKey:identifier];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"exception: %@", exception);
+    }
 }
 
 - (NSData *)cachedImageDataWithIdentifier:(NSString *)identifier {
     if (!identifier && identifier.length<1) return nil;
-    NSData *cachedImageData = [_cache objectForKey:identifier];
-    return cachedImageData;
+    @try {
+        NSData *cachedImageData = [_cache objectForKey:identifier];
+        return cachedImageData;
+    }
+    @catch (NSException *exception) {
+        NSLog(@"exception: %@", exception);
+    }
+    return nil;
 }
 
 @end
