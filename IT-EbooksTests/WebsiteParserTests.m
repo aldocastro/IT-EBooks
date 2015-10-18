7a5a32e6b6b786f3ba996cb3fd37f798cb1d7af2
Aldo Castro <aldobest@Aldos-MacBook-Pro.local>
2015-10-19 01:54:20 +0200

    adding unit test for WebsiteParser class

---
+++ IT-EbooksTests/WebsiteParserTests.m
@@ -0,0 +1,39 @@
+//
+//  WebsiteParserTests.m
+//  IT-Ebooks
+//
+//  Created by Aldo Castro on 17/10/15.
+//  Copyright © 2015 Aldo Castro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebsiteParserTests : XCTestCase
+
+@end
+
+@implementation WebsiteParserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
