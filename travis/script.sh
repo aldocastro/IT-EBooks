c8811bb37135c51197fa7240b4c60b177aa61f32
Aldo Castro <castro.aldo@live.com>
2015-12-05 20:06:01 +0100

    updating scripts

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-xctool -workspace IT-Ebooks.xcworkspace -scheme IT-Ebooks
-xctool test -workspace IT-Ebooks.xcworkspace -scheme IT-EbooksTests
+xctool -workspace IT-Ebooks.xcworkspace -scheme IT-Ebooks -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+xctool test -workspace IT-Ebooks.xcworkspace -scheme IT-EbooksTests -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
