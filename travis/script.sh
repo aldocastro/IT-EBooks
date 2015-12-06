a979c7e401d4b07a76faa3f19ef667f80f7310e6
Aldo Castro <castro.aldo@live.com>
2015-12-06 09:26:10 +0100

    adding line to make a realease build

--- travis/script.sh
+++ travis/script.sh
@@ -3,3 +3,4 @@ set -ev
 
 xctool -workspace IT-Ebooks.xcworkspace -scheme IT-Ebooks -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
 xctool test -workspace IT-Ebooks.xcworkspace -scheme IT-EbooksTests -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+# xctool -workspace IT-Ebooks.xcworkspace -scheme IT-Ebooks -configuration Release build archive
